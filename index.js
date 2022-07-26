7cd3d7cfff4cae473ec6017b40da6e92c694b055
Desifresh <neilydraco94@gmail.com>
2022-07-25 22:35:41 -0400

    7.25.22

---
+++ index.js
@@ -0,0 +1,21 @@
+var helloworld="Hello World";
+
+
+console.log(helloworld);
+console.log("i think i get it");
+
+var boolean=true;
+var number=9.2;
+console.log(helloworld + boolean + number);
+console.log(boolean + number);
+
+function adderfunction(){
+    var number=9;
+    number = number + 1;
+    number *= number;
+    return number;
+
+}
+
+console.log(adderfunction());
+
